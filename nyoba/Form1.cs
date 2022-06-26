a6b2b7282c9d7458e9244511fdde16f262a94115
cihuyyama <miqbalalhabib@gmail.com>
2022-06-26 21:29:33 +0700

    Add project files.

---
+++ nyoba/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nyoba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-UP691C2\IQBAL;Initial Catalog=tratix;Integrated Security=True");
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from account where username='"+ textusername.Text +"' and password='"+ textpass.Text +"'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                if (rd.HasRows)
+                {
+                    this.Hide();
+                    con.Close();
+                    Form3 a = new Form3();
+                    a.Show();
+                }
+
+                MessageBox.Show("Login Berhasil", "Disimpan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool IsValid()
+        {
+            if (textusername.Text == String.Empty)
+            {
+                MessageBox.Show("Username salah", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 a = new Form2();
+            a.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textpass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textusername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ nyoba/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nyoba
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-UP691C2\IQBAL;Initial Catalog=tratix;Integrated Security=True");
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO account VALUES (@username, @password)", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@username", textusername.Text);
+                cmd.Parameters.AddWithValue("@password", textpass.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Data baru telah ditambahkan", "Disimpan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private bool IsValid()
+        {
+            if (textusername.Text == String.Empty)
+            {
+                MessageBox.Show("Username tidak boleh kosong", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 a = new Form1();
+            a.Show();
+        }
+    }
+}
